6b72e3998963a30b020a6b8b84bdafdb200cea9f
Mary Mihn <mary.moss333@gmail.com>
2022-06-13 00:18:03 +0300

    feat: add 1 lambda function

---
+++ getProducts/handler.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports.hello = async (event) => {
+
+const products = [  {
+  "count": 1,
+  "description": "Betrayal at House on the Hill 3rd Edition",
+  "id": "1",
+  "price": 35,
+  "title": "Betrayal at House on the Hill 3rd Edition"
+},
+{
+  "count": 6,
+  "description": "Rustling Leaves",
+  "id": "2",
+  "price": 68,
+  "title": "Rustling Leaves"
+},
+{
+  "count": 1,
+  "description": "So Clover!",
+  "id": "3",
+  "price": 359,
+  "title": "So Clover!"
+},
+{
+  "count": 1,
+  "description": "Quacks of Quedlinburg- Herb Witches",
+  "id": "4",
+  "price": 8,
+  "title": "Quacks of Quedlinburg- Herb Witches"
+},
+]
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(products),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
